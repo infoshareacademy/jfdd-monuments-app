1c330c4a735511e2e7b116f31a22520f6edefeb1
GGoo <piekarska@live.com>
2016-01-19 17:14:50 +0100

    js map

---
+++ scripts/map.js
@@ -0,0 +1,22 @@
+/**
+ * Created by gosia on 19.01.16.
+ */
+
+$(document).ready(function() {
+
+    function initialize() {
+        var mapCanvas = document.getElementById('map');
+
+        var mapOptions = {
+            center: new google.maps.LatLng(44.5403, -78.5463),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+});
+
