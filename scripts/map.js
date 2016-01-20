135a2a5474f59e24228a78779745a245ff411cc1
GGoo <piekarska@live.com>
2016-01-20 18:58:48 +0100

    mmm

--- scripts/map.js
+++ scripts/map.js
@@ -3,19 +3,16 @@
  */
 
 $(document).ready(function() {
-
     function initialize() {
         var mapCanvas = document.getElementById('map');
-
         var mapOptions = {
+            //center: new google.maps.LatLng(44.5403, -78.5463),
             center: new google.maps.LatLng(44.5403, -78.5463),
-            zoom: 10,
+            zoom: 7,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-
         var map = new google.maps.Map(mapCanvas, mapOptions);
     }
-
     google.maps.event.addDomListener(window, 'load', initialize);
 
 });
