73e73d3db9f93b02b13ce4ffa6bd933e6ff71419
lukasz184 <deput.lukasz@gmail.com>
2016-01-27 15:44:56 +0100

    info o zabytkach pocz

---
+++ scripts/info_zabytki.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function() {
+    $('.wywolanie__info__zabytki').click(function(){
+        $('.form__info__zabytki').toggle(500);
+    });
+
+    $('.close__info__zabytki').click(function(){
+       $('.form__info__zabytki').toggle(500);
+    });
+
+
+
+
+
+}
