336ea251fa693213a97fb79b9031130f04b2ab08
lukasz184 <deput.lukasz@gmail.com>
2016-01-28 16:42:20 +0100

    ajax

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,50 +1,50 @@
 /**
  * Created by gosia on 21.01.16.
  */
+
 function showMonumentsWithGeographicalData(data){
-
-
     data = JSON.parse(data);
     console.log(data);
-    data.filter(function (monument) {
-        return  monument.Obiekt  && monument.Ulica && monument.Nr && monument.Dlugosc && monument.Szerokosc;
-    }).map(function(monument) {
-        var html = '';
-        html += '<td>' + monument.Obiekt + '</td>';
-        html += '<td>' + monument.Ulica + '</td>';
-        html += '<td>' + monument.Nr + '</td>';
-        return html;
-    }).forEach(function (htmlPart) {
-        $('.rzeczywisteDane').append(
-            '<tr data-role="Dane z rzeczywistego API">' +
-            htmlPart +
-            '</tr>'
-        );
+
+    //data.sort(function (Obiekt){
+    //    return parseString(ASC).Obiekt;
+    //});
+
+
+    var monumentsWithCords = data.filter(function (monument) {
+        return  monument.Dlugosc && monument.Szerokosc;
     });
 
-}
+    var map;
+    var lat = 54.3485481;
+    var lng = 18.6510855;
+    var zoom = 14;
 
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    var myOptions = {
+        zoom: zoom,
+        center: myLatlng,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        draggable: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
 
-function fetchZabytki() {
-    console.log('fetch zabytki');
-    $.ajax({
-        url: 'data/dane-zabytkow.csv',
-        success: function(csvZabytki) {
-            //console.log('FETCHED', csvZabytki);
+    var markerInstances = monumentsWithCords.map(function (monument) {
+        var image = 'images/blue_MarkerZ.png';
+        var myLatLng = new google.maps.LatLng(monument.Dlugosc, monument.Szerokosc);
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: monument.Obiekt,
+            icon: image
+        });
 
-            var daneZabytkow = CSV2JSON (csvZabytki, ',');
+        return marker;
+    });
 
-            //console.log(daneZabytkow);
-
-            showMonumentsWithGeographicalData(daneZabytkow);
-
-            //wyswietlenie w konsoli wszytich zabytkow (f12)
-        },
-        error: function(err, std) {
-            console.debug('ERROR', err, std);
-        }
-    })
-}
-$(document).ready(function() {
-    fetchZabytki();
-});
+    markerInstances.forEach(function (marker) {
+        marker.setMap(map);
+    });
