68269a162f0d3b757bf65dec01c7bb4c6860ba47
lukasz184 <deput.lukasz@gmail.com>
2016-02-01 01:01:57 +0100

    zmienione json-y pod zmienione zabytki

--- scripts/moj-json.js
+++ scripts/moj-json.js
@@ -2,7 +2,7 @@
  * Created by lukaszd on 27.01.16.
  */
 
-var Monuments_Ids = {66: 'wybierz-zabytek', 0: 'bazylika', 1: 'brama-ducha', 2: 'brama-zuraw', 3: 'dom-uphagena', 4:'dlugi-targ', 5: 'fontanna', 6: 'kanal', 7: 'muzeum-arch', 8: 'stoczniowcy', 9: 'ratusz'};
+var Monuments_Ids = {66: 'wybierz-zabytek', 0: 'bazylika', 1: 'brama-wyzynna', 2: 'brama-ducha', 3: 'dom-uphagena', 4:'kanal', 5: 'muzeum-narodowe', 6: 'stoczniowcy', 7: 'ratusz', 8: 'reduta', 9: 'westerplatte', 10: 'wyspa'};
 
 // funkcja wyświetlająca info o zabytkach poprzez button
 
