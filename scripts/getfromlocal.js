5be363459519f5198097276ea6e56f1f4061e880
lukasz184 <deput.lukasz@gmail.com>
2016-02-03 16:38:46 +0100

    local storage pocz

---
+++ scripts/getfromlocal.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lukaszd on 03.02.16.
+ */
+
+function setItemToLocalStorage() {
+    localStorage.setItem(key, JSON.stringify(data)
+    )}
+
+function getItemFromLocalStorage (key, data) {
+    return
+}
