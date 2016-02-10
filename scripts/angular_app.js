095ea541892f33db82ec8a02e2d0b0d41bdfa60a
KrzysztofGolat <kgolat@gmail.com>
2016-02-10 17:29:15 +0100

    Old version of monuments page added by angular.

--- scripts/angular_app.js
+++ scripts/angular_app.js
@@ -1,25 +1,25 @@
-//(function(){
-//    var app = angular.module('Workshop', []);
-//
-//    app.controller('BaseController', function () {
-//
-//    });
-//})();
+(function(){
+    var app = angular.module('Workshop', []);
 
+    //app.controller('BaseController', function () {    });
 
+    //angular.module('Workshop')
+        app.directive('staraApka', function () {
+            return {
+                restrict: 'EAC',
+                templateUrl: 'index_old.html'
 
+                //transclude: true,
+                //scope: {},
+                //controller: function ($scope, $http) {
+                //    $http.get('http://www.wp.pl').then(function (response) {
+                //        $scope.loginData = response.data;
+                //    });
+                //}
 
-angular.module('Workshop')
-    .directive('Workshop', function () {
-        return {
-            restrict: 'EAC',
-            templateUrl: 'index_LS.html',
-            transclude: true,
-            scope: {},
-            controller: function ($scope, $http) {
-                $http.get('index.html').then(function (response) {
-                    $scope.loginData = response.data;
-                });
             }
-        }
-    });
+        });
+})();
+
+
+
