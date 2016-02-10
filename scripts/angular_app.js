aa66c41a2be16e42c9042c7793cdd34ee4d4af4f
Krzysztof <kgolat@gmail.com>
2016-02-10 15:10:17 +0100

    Little changes in Angular.

--- scripts/angular_app.js
+++ scripts/angular_app.js
@@ -1,7 +1,25 @@
-(function(){
-    var app = angular.module('Workshop', []);
+//(function(){
+//    var app = angular.module('Workshop', []);
+//
+//    app.controller('BaseController', function () {
+//
+//    });
+//})();
 
-    app.controller('BaseController', function () {
 
+
+
+angular.module('Workshop')
+    .directive('Workshop', function () {
+        return {
+            restrict: 'EAC',
+            templateUrl: 'index_LS.html',
+            transclude: true,
+            scope: {},
+            controller: function ($scope, $http) {
+                $http.get('http://www.wp.pl').then(function (response) {
+                    $scope.loginData = response.data;
+                });
+            }
+        }
     });
-})();
