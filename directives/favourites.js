2296e57f03d6358e71395a4a3a24e088ed01d9d4
lukasz184 <deput.lukasz@gmail.com>
2016-02-11 18:04:44 +0100

    drag & drop duzo zrobione

--- directives/favourites.js
+++ directives/favourites.js
@@ -2,37 +2,54 @@
  * Created by lukaszd on 10.02.16.
  */
 
-angular.module('Workshop')
+angular.module('Workshop', ['dndLists'])
     .directive('favorites', function () {
         return {
             restrict: 'E',
             templateUrl: 'directives/favorites.html',
             transclude: true,
             scope: {},
-            controller: function ($scope, $http) {
-                $http.get('').then(function (response) {
+            controller: function ($scope) {
+
+                $scope.list = [];
+
+
+                $scope.updateFavorites = function () {
+                    localStorage.setItem(
+                        'ulubione', $scope.models.lists.ulubione
+                    );
+                };
+
+                $scope.models = {
+                    selected: null,
+                    lists: {
+                        "zabytki": [],
+                        "ulubione": []
+                    }
+                };
+
+                if (localStorage.getItem('ulubione')) $scope.models.lists.ulubione = localStorage.getItem('ulubione').split(',');
+
+                $scope.models.lists.zabytki = ['wybierz-zabytek', 'bazylika', 'brama-wyzynna', 'brama-ducha', 'dom-uphagena', 'kanal', 'muzeum-narodowe', 'stoczniowcy', 'ratusz', 'reduta', 'westerplatte', 'wyspa'];
+
+                for (var i = 0; i < $scope.models.lists.zabytki.length; i++) {
+                    $scope.models.lists.ulubione.forEach(function (ul) {
+                            if (ul == $scope.models.lists.zabytki[i]) {
+                                $scope.models.lists.zabytki.splice(i,1);
+                            }
+                        }
+                    )
+                }
+
+
+                // Model to JSON for demo purpose
+
+                //$http.get('').then(function (response) {
                 //    $scope.loginData = response.data;
                 //});
 
-                $scope.models = {
-                selected: null,
-                lists: {"zabytki": [], "ulubione": []}
-                };
 
-            // Generate initial model
-                for (var i = 1; i <= 10; ++i) {
-                    $scope.models.lists.zabytki.push({label: "Item A" + i});
-                    $scope.models.lists.ulubione.push({label: "Item B" + i});
-                }
-
-            // Model to JSON for demo purpose
-            $scope.$watch('models', function (model) {
-                $scope.modelAsJson = angular.toJson(model, true);
-            }, true);
-
-
-
-                })
+                //})
             }
         }
     });
@@ -42,26 +59,3 @@ angular.module('Workshop')
 
 
 
-//(function () {
-//    angular
-//        .module("Favorites", [])
-//        .controller('BaseController2', function ($scope) {
-//
-//            $scope.models = {
-//                selected: null,
-//                lists: {"zabytki": [], "ulubione": []}
-//            };
-//
-//            // Generate initial model
-//            for (var i = 1; i <= 10; ++i) {
-//                $scope.models.lists.zabytki.push({label: "Item A" + i});
-//                $scope.models.lists.ulubione.push({label: "Item B" + i});
-//            }
-//
-//            // Model to JSON for demo purpose
-//            $scope.$watch('models', function (model) {
-//                $scope.modelAsJson = angular.toJson(model, true);
-//            }, true);
-//
-//        });
-//})();
