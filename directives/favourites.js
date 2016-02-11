a843e491ffbcc5d203eac40cef21e1cf5a6c3202
lukasz184 <deput.lukasz@gmail.com>
2016-02-11 01:29:31 +0100

    drag&drop angular pod dyrektywe cd

--- directives/favourites.js
+++ directives/favourites.js
@@ -11,8 +11,28 @@ angular.module('Workshop')
             scope: {},
             controller: function ($scope, $http) {
                 $http.get('').then(function (response) {
-                    $scope.loginData = response.data;
-                });
+                //    $scope.loginData = response.data;
+                //});
+
+                $scope.models = {
+                selected: null,
+                lists: {"zabytki": [], "ulubione": []}
+                };
+
+            // Generate initial model
+                for (var i = 1; i <= 10; ++i) {
+                    $scope.models.lists.zabytki.push({label: "Item A" + i});
+                    $scope.models.lists.ulubione.push({label: "Item B" + i});
+                }
+
+            // Model to JSON for demo purpose
+            $scope.$watch('models', function (model) {
+                $scope.modelAsJson = angular.toJson(model, true);
+            }, true);
+
+
+
+                })
             }
         }
     });
